d1efe0335131c2b3a1b79a8280208cdf82896a66
Moussa DIALLO <diallomous@gmail.com>
2023-08-04 21:44:42 +0000

    removing import seaborn on cleanData

--- cleanData.py
+++ cleanData.py
@@ -3,7 +3,7 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression, Ridge
 import matplotlib.pyplot as plt
-import seaborn as sns
+#import seaborn as sns
 from sklearn.preprocessing import OneHotEncoder
 import zipfile
 
